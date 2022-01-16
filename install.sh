5e35218d979881ffdb77cf6b0df0d4e63725193d
irfanhasib0 <irfanhasib.me@gmail.com>
2022-01-16 22:27:28 +0900

    saving progress

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 sudo apt-get install python3.8 python3-pip
 pip3 install virtualenv
-#python3 -m virtualenv python_38
+python3 -m virtualenv python_38
 source python_38/bin/activate
 #sudo ./cuda.sh
 pip3 install -r requirements.txt
+sudo ln -s /usr/local/cuda-11.4/targets/x86_64/lib/libcusolver.so.11 $PWD/python_38/lib/python3.8/site-packages/tensorflow/python/libcusolver.so.10
 pip3 install torch==1.10.1+cu113 torchvision==0.11.2+cu113 torchaudio==0.10.1+cu113 -f https://download.pytorch.org/whl/cu113/torch_stable.html
---
+++ start.sh
@@ -0,0 +1,2 @@
+#/!bin/bash
+source ./Mlops-Env/python_38/bin/activate && jupyter lab --port 8001 
